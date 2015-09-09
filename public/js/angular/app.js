ea6b316912e41b62cd0607fbe0e8ae8da70330e4
ianstalter123 <istalter66@gmail.com>
2015-09-09 13:29:12 -0700

    add screenshot

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -2,14 +2,7 @@ var app = angular.module('BbApp', ['ngRoute'])
 
 .controller('AppController', ['$scope', '$http', function($scope, $http) {
 
-	$http.get('/data')
-		.success(function(data) {
-			$scope.test = data;
-			console.log(data)
-		})
-		.error(function(data) {
-			console.log('Error: ' + data);
-		});
+	
 
 }])
 
